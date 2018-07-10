20e8435bd61058768670e6ca3c62cb7208df8915
Daniela Munoz <daniela.munoz@enova.mx>
2018-07-10 12:32:15 -0500

    Feature | Creating a date countdown at home component
    Feature | Adding css styles to countdown component
    
    Feature | Adding styles to home component
    
    Feature | Moving countdown to a separate component
    
    - Home countdown moved to a separate component
    - Fixing scss styles
    - Responsive styles added for home and countdown
    
    Feature | Wedding detail information section added to homepage
    
    Feature | Adding details for the wedding on home component
    x
    
    Feature | Replacing p tags from countdown component
    
    - P tags are replaced for spans with clases
    
    Feature | Fixing comments from pull request
    
    - Adding a class to the span that contains an ornament image in the main cover
    - Comments in typography scss file are removed
    - Font variable removed since it was not being used in the proyect
    - Color variable name changed from 'sec-color' to 'secondary-color'
    - Font variable name changed to english

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,21 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRouting } from './home.routing';
-import { HomeComponent } from './components/home.component';
+import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from '../shared/components/header/header.component';
 import { FooterComponent } from '../shared/components/footer/footer.component';
+import { CountdownComponent } from './components/countdown/countdown.component';
 
 
 @NgModule({
-	imports: [
-		CommonModule,
-		HomeRouting
-	],
-	declarations: [
-		HomeComponent,
-		HeaderComponent,
-		FooterComponent
-	]
+    imports: [
+        CommonModule,
+        HomeRouting
+    ],
+    declarations: [
+        HomeComponent,
+        HeaderComponent,
+        FooterComponent,
+        CountdownComponent
+    ]
 })
 
 
