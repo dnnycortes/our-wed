c2feacce9a65a6495187a91b67a7f26c33a45185
Daniela Cortes <daniela.munozc@gmail.com>
2018-10-11 18:01:57 -0500

    Feature | Making adjustments to landing page (#11)
    * Feature | Making adjustments to landing page
    
    - Adding menu styles when scrolling
    - Adding links to section into nav
    - Adjustments added for cover
    - Responsive styles added
    
    * Feature | Adding address into variables
    
    - Adding address for reception and ceremony in variables

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 
 
 @Component({
@@ -8,4 +8,25 @@ import { Component } from '@angular/core';
 })
 
 
-export class HomeComponent { }
+export class HomeComponent implements OnInit {
+    public ceremony: String;
+    public reception: String;
+    @ViewChild('saveTheDate') saveTheDate: ElementRef;
+    @ViewChild('location') location: ElementRef;
+
+    scrollTo( event ) {
+        switch ( event ) {
+            case 'save-the-date':
+                this.saveTheDate.nativeElement.scrollIntoView( { behavior: 'smooth', block: 'start' } );
+                break;
+            case 'location':
+                this.location.nativeElement.scrollIntoView( { behavior: 'smooth', block: 'start' } );
+                break;
+        }
+    }
+
+    ngOnInit() {
+        this.ceremony = 'https://goo.gl/maps/g4aJJs5isrF2';
+        this.reception = 'https://goo.gl/maps/dXfvajwyPR62';
+    }
+}
